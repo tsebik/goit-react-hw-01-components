9af4f47e6424e05a2d3c30a8f1eb22110853253f
Дмитрий Цебинога <tsebik@ukr.net>
2022-09-12 20:37:53 +0300

    hw-1

--- src/components/Friends/FriendListItem.jsx
+++ src/components/Friends/FriendListItem.jsx
@@ -4,12 +4,11 @@ import css from 'components/Friends/Friends.module.css'
 const FriendListItem = ({ status, avatar, name }) => {
 
 	return <li className={css.item}>
-		<span
-                className={css.status}
-                style={{
-                  backgroundColor: status ? '#3cb371' : '#ff0000',
-                }}
-              ></span>
+		{status
+			? <span className={css.status}></span> 
+			: <span className={css.statusFalse}></span>
+		}
+		
   		<img className={css.avatar} src={avatar} alt="User avatar" width="48" />
 		<p className={css.name}>{name}</p>
 </li>
